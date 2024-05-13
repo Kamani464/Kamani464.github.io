91589e041489cdd07fc5b4728d2cd8fd26d35e97
Kamani464 <93107048+Kamani464@users.noreply.github.com>
2024-05-13 13:08:19 -0400

    fixed alot

--- Final/script.js
+++ Final/script.js
@@ -1,7 +1,51 @@
-	<script>
-        document.getElementById("startButton").addEventListener("click", function() {
-        var playerName = document.getElementById("nameInput").value;
-        localStorage.setItem("playerName", playerName);
-        window.location.href = "Page1.html"; 
-        });
-    </script>
+
+if (window.location.pathname.includes("Page7.html")) {
+    setTimeout(redirectToPage1D, 5000);
+}
+
+var theDiv = document.getElementById("helloDiv");
+
+function playAudio(url) {
+  new Audio(url).play();
+}
+function sayHello() {
+    var fNameVar = document.getElementById("fName").value;
+    var lNameVar = document.getElementById("lName").value;
+    theDiv.innerHTML = "Hello " + fNameVar + " " + lNameVar + "!";
+}
+
+function mouseOverFUNC() {
+    document.getElementById("goon").src = "Resources/SadnessH.jpg";
+}
+
+function mouseOutFUNC() {
+    document.getElementById("goon").src = "Resources/Sadness.jpg";
+}
+
+function mouseDownFUNC() {
+    document.getElementById("goon").src = "Resources/Sadness2.jpg";
+}
+
+function mouseOverFUNC() {
+    document.getElementById("break").src = "Resources/Breakit.png";
+}
+
+function mouseOutFUNC() {
+    document.getElementById("break").src = "Resources/Carlton2.jpg";
+}
+
+function mouseDownFUNC() {
+    document.getElementById("break").src = "Resources/Broken.png";
+}
+
+
+function redirectToPage1D() {
+    window.location.href = "PageD1.html";
+}
+
+function displayWinMessage() {
+    console.log("Clicked on the image!");
+    if (confirm("YOU DID IT!! YOU FOUND THE BEAR! Back to home?")) {
+        window.location.href = "index.html"; 
+    }
+}
